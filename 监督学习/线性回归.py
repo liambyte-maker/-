e5835eb2928dbb4e154dc9fc1e7daed40dda46fb
liam <l2258054087@126.com>
2026-02-24 09:23:41 +0800

    Update 线性回归.py

--- 监督学习/线性回归.py
+++ 监督学习/线性回归.py
@@ -1,1 +1,16 @@
 
+From sklean.linear_model import LinearRegression
+通过找到合适的w和b，使得误差最小
+最小二乘法 least square method
+[图片]
+梯度下降法 gradient descent：
+[图片]
+对w，b求偏导
+更新w和b
+[图片]
+1.初始化w和b的值
+2.计算损失函数
+3.计算梯度
+4.根据梯度更新b和w
+5.重复2到4，直到损失函数收敛或达到最大迭代次数
+Score 
